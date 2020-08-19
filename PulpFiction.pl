eecdd1d3b0e8dd8d9ddcca80a87445c38740a07c
Raquel La Cruz <raquellacruz06@gmail.com>
2020-08-19 18:05:30 -0300

    Punto 6 sin terminar

--- PulpFiction.pl
+++ PulpFiction.pl
@@ -171,6 +171,34 @@ estaEnLaTarea(Personaje, cuidar(Personaje)).
 estaEnLaTarea(Personaje, ayudar(Personaje)).
 estaEnLaTarea(Personaje, buscar(Personaje, _)).
 
+/*Punto 8
+Ah, algo más: nuestros personajes tienen características. Lo cual es bueno, porque nos ayuda a
+ diferenciarlos cuando están de a dos. Por ejemplo:
+
+Desarrollar duoDiferenciable/2, que relaciona a un dúo (dos amigos o una pareja) en el que uno 
+tiene al menos una característica que el otro no. */
+
+caracteristicas(vincent,  [negro, muchoPelo, tieneCabeza]).
+caracteristicas(jules,    [tieneCabeza, muchoPelo]).
+caracteristicas(jimmie,   [negro]).
+
+duoDiferenciable(Persona, OtraPersona):-
+    seRelacionan(Persona, OtraPersona),
+    tienenCaracteristicaDistinta(Persona, OtraPersona).
+
+/*tienenCaracteristicaDistinta(Persona, OtraPersona):-
+    hallarCaracteristica(Persona, Caracteristica),
+    hallarCaracteristica(OtraPersona, OtraCaracteristica),
+    Caracteristica \= OtraCaracteristica.*/
+
+hallarCaracteristica(Persona, Caracteristica):-
+    caracteristicas(Persona, Caracteristicas),
+    member(Caracteristica, Caracteristicas).
+
+tienenCaracteristicaDistinta(Persona, OtraPersona):-
+    hallarCaracteristica(Persona, Caracteristica),
+    caracteristicas(OtraPersona, OtrasCaracteristicas),
+    not(member(Caracteristica, OtrasCaracteristicas)).
 
 
 
